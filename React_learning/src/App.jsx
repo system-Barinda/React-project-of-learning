18818e331defcf5f3aa065bdd876734649e30c35
system sylvere BARINDA <systembarinda@gmail.com>
2026-01-29 09:50:54 +0200

    done to make form

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
-
+import FormValidation from './page/form';
 
 import './App.css'
 
+function App(){
+  return (
+    <>
+     <FormValidation />
+    </>
+  );
+}
 
+export default App;
---
+++ React_learning/src/page/form.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function FormValidation (){
+    return(
+        <>
+        <div className="container h-150 w-100 shadow-2xl bg-sky-200 text-black mx-auto">
+
+        </div>
+        </>
+    );
+}
+export default FormValidation;
