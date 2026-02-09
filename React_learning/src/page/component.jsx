90c1fb1c30159fdc202f23894a1210c98e2fdf2f
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-09 19:13:43 +0200

    done

--- React_learning/src/page/component.jsx
+++ React_learning/src/page/component.jsx
@@ -427,10 +427,7 @@ import  { useState } from "react";
 // }
 
 function Square({value,onSquareClick}){
-   // const [value,setValue] = React.useState(null);
-    // const onSquareClick = () => {
-    //     setValue("X")
-    // }
+  
  return <button onClick={onSquareClick} className="square h-20 w-20 m-5 border">{value}</button>
 }
 export default function Board(){
@@ -443,20 +440,20 @@ export default function Board(){
     return(
        <>
       <div className="board-row h-30 w-100 border">
-        <Square  value={squares[0]} onSquareClick={handleClick} />
-        <Square  value={squares[1]} />
-        <Square  value={squares[2]} />
+        <Square  value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square  value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square  value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row h-30 w-100 border">
-        <Square  value={squares[3]} />
-        <Square  value={squares[4]} />
-        <Square  value={squares[5]} />
+        <Square  value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square  value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square  value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
       <div className="board-row h-30 w-100 border">
-        <Square  value={squares[6]} />
-        <Square  value={squares[7]} />
-        <Square  value={squares[8]} />
+        <Square  value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square  value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square  value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
 
       
