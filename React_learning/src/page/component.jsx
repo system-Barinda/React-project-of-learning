9e5d6a7727cb7ad7998dc308883e90169d45c460
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-10 17:49:06 +0200

    done

--- React_learning/src/page/component.jsx
+++ React_learning/src/page/component.jsx
@@ -549,79 +549,65 @@ import  { useState } from "react";
 //   }
 //   return null;
 // }
- const products = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-];
-
-export default function App(){
-return(
-  <FilterableProductTable products={products} />
-)
-};
-
-function FilterableProductTable({products}){
- return(
-  <div>
-    <SearchBar />
-    <ProductTable products={products} />
-  </div>
- )
-}
-
-function ProductTable({products}){
-const rows = [];
-let lastCategory = null;
-
-products.forEach((product) => {
-  if(product.category !==  lastCategory){
-    rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
-  }
-  lastCategory = product.category;
-})
-return(
-  <table className="border">
-    <thead>
-      <tr>
-        <th>name</th>
-        <th>price</th>
-      </tr>
-    </thead>
-     <tbody>{rows}</tbody>
-  </table>
-)
-}
-
-
-
-function ProductCategoryRow({category}){
- return(
-  <tr>
-    <th colSpan="2">{category}</th>
-  </tr>
- );
-}
-
-
-
-function ProductRow({product}){
-  const name = product.stocked ? product.name :
-  <span style={{color:'red'}}>{product.name}</span>
-  
- return(
-  <tr>
-    <td>{name}</td>
-    <td>{product.price}</td>
+ function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
     </tr>
- )
+  );
 }
 
-function SearchBar(){
-  return(
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
     <form>
       <input type="text" placeholder="Search..." />
       <label>
@@ -630,5 +616,27 @@ function SearchBar(){
         Only show products in stock
       </label>
     </form>
-  )
+  );
+}
+
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
 }
