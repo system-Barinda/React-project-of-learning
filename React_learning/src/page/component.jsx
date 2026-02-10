08b5f57e3279a95cc945fc94358cf990913ef191
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-10 17:31:16 +0200

    done

--- React_learning/src/page/component.jsx
+++ React_learning/src/page/component.jsx
@@ -566,12 +566,25 @@ function ProductTable(){
 
 }
 
-function ProductCategoryRow(){
-
-}
-function ProductRow(){
-
+function ProductCategoryRow({category}){
+ return(
+  <tr>
+    <th colSpan="2">{category}</th>
+  </tr>
+ );
 }
 
   return <button>send data</button>
 };
+
+function ProductRow({product}){
+  const name = product.stocked ? product.name :
+  <span style={{color:'red'}}>{product.name}</span>
+  
+ return(
+  <tr>
+    <td>{name}</td>
+    <td>{product.price}</td>
+    </tr>
+ )
+}
