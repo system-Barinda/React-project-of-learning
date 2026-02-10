e412c1b2228209296d81e819cb5de2267a69d928
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-10 17:39:58 +0200

    done

--- React_learning/src/page/component.jsx
+++ React_learning/src/page/component.jsx
@@ -612,3 +612,16 @@ function ProductRow({product}){
     </tr>
  )
 }
+
+function SearchBar(){
+  return(
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  )
+}
