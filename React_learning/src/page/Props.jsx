5c4fe474ce3eeb71bec4874fa812d3bc36c298ad
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-13 18:26:40 +0200

    all done to fetch data

--- React_learning/src/page/Props.jsx
+++ React_learning/src/page/Props.jsx
@@ -1,8 +1,25 @@
 import { useState,useEffect } from "react"
 import Cards from "../components/Cards"
 
+const url = "https://fakestoreapi.com/products";
 export default function DesplayData(){
 const[product,setProduct] = useState({});
+
+useEffect(() => {
+    const getData = async() => {
+        try{
+            const response = await fetch(url);
+            if(! response.ok) throw new Error('network connection failed');
+            const data = await response.json();
+            setProduct(data);
+        }
+        catch(err) {
+            return new Error( err instanceof Error ? err : {
+                errname:"something went wrong",
+            why:err.reason});
+        }
+    }
+})
     return (
         <Cards  />
     )
