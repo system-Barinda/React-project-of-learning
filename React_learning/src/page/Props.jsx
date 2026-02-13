b882d8c6b67932f991e9800d0a5b799592e7c3da
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-13 18:21:07 +0200

    I'm going to start the fetch all data in props file

--- React_learning/src/page/Props.jsx
+++ React_learning/src/page/Props.jsx
@@ -1,6 +1,8 @@
+import { useState,useEffect } from "react"
 import Cards from "../components/Cards"
 
 export default function DesplayData(){
+const[product,setProduct] = useState({});
     return (
         <Cards  />
     )
