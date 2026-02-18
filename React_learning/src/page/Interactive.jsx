8042a6fda77a146cd54272784c52c1ad5c42fede
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 11:01:10 +0200

    done

--- React_learning/src/page/Interactive.jsx
+++ React_learning/src/page/Interactive.jsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export default function Interactive() {
-  const [data, setData] = useState([]);
-  const name = "barinda system sylvere";
+// export default function Interactive() {
+//   const [data, setData] = useState([]);
+//   const name = "barinda system sylvere";
 
-  const handleButton = () => {
-    setData(prev => [...prev, name]); 
-  };
+//   const handleButton = () => {
+//     setData(prev => [...prev, name]); 
+//   };
 
-  return (
-    <div>
-      <button onClick={handleButton}>add barinda</button>
+//   return (
+//     <div>
+//       <button onClick={handleButton}>add barinda</button>
 
-      {data.map((el, index) => (
-        <button key={index}>{el}</button>
-      ))}
-    </div>
-  );
+//       {data.map((el, index) => (
+//         <button key={index}>{el}</button>
+//       ))}
+//     </div>
+//   );
+// }
+
+export default function Interactive(){
+    const myRef = useRef(initiation);
+    console.log(myRef)
+    return(
+        <>
+        </>
+    )
 }
