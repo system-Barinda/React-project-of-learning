e4e38770056549a67b627a333c45255e6c7fef86
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 15:58:37 +0200

    done'

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import LightSwitch from "./page/LightSwitch"
+import LightSwitch from "./page/Signup"
 
 import './App.css'
 
---
+++ React_learning/src/page/Signup.jsx
@@ -0,0 +1,8 @@
+export default function Signup(){
+    return(<>
+       <form action="">
+        <input type="text" className=" border" />
+        <button type="submit">send</button>
+       </form>
+    </>)
+}
