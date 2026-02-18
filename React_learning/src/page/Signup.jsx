67f01100d760673cfcf89e5929973fc6df2e094a
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 16:03:56 +0200

    don

--- React_learning/src/page/Signup.jsx
+++ React_learning/src/page/Signup.jsx
@@ -1,8 +1,22 @@
-export default function Signup(){
-    return(<>
-       <form action="">
-        <input type="text" className=" border" />
-        <button type="submit">send</button>
-       </form>
-    </>)
+export default function Signup() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("barinda");
+  }
+
+  function handleInp(e) {
+    console.log(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={handleInp}
+        className="border"
+      />
+      <button type="submit">send</button>
+    </form>
+  );
 }
