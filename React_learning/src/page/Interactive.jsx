e1523a743ad64fa464b9496ec7dab548812b19ae
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-18 10:44:53 +0200

    done

--- React_learning/src/page/Interactive.jsx
+++ React_learning/src/page/Interactive.jsx
@@ -1,1 +1,11 @@
 
+export default function Interactive(){
+    const handlebutton = () => {
+        console.log('my name is barinda system sylvere');
+    }
+    return(
+        <>
+          <button onClick={handlebutton}>add barinda</button>
+        </>
+    )
+}
