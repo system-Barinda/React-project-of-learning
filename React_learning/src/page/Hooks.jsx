709378e1654a4ad6c1ce618367225fecdbb5ab96
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-19 11:13:42 +0200

    done

--- React_learning/src/page/Hooks.jsx
+++ React_learning/src/page/Hooks.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react"
+import { useState,useCallback } from "react"
 
-function Button ({onClick}){
-    console.log("buttone are rerendered");
-    return <Button onClick={onClick}>clicked me</Button>
-}
+// function Button ({onClick}){
+//     console.log("buttone are rerendered");
+//     return <Button onClick={onClick}>clicked me</Button>
+// }
+// export default function Hooks(){
+//     const[count,setCount] = useState(0);
+//     const handleClick = () => {
+//         console.log('clicked');
+//     }
+//     return(
+//         <>
+//         <h1>{count}</h1>
+//          <button onClick={handleClick} />
+//          <button onClick={() => setCount(count + 1)}>click</button>
+//         </>
+//     )
+// }
+
+
 export default function Hooks(){
-    const[count,setCount] = useState(0);
-    const handleClick = () => {
-        console.log('clicked');
-    }
-    return(
-        <>
-        <h1>{count}</h1>
-         <button onClick={handleClick} />
-         <button onClick={() => setCount(count + 1)}>click</button>
-        </>
-    )
+
+    const button = React.memo(({onClick}) => {
+    console.log("buttone rendered");
+    return <button onClick={onClick}>clicked me</button>
+    });
+    
+    return()
 }
