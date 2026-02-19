687fdf3cd49e388c535deaec305b3249c9f7ac02
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-19 11:09:46 +0200

    done

--- React_learning/src/page/Hooks.jsx
+++ React_learning/src/page/Hooks.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+
+function Button ({onClick}){
+    console.log("buttone are rerendered");
+    return <Button onClick={onClick}>clicked me</Button>
+}
 export default function Hooks(){
+    const[count,setCount] = useState(0);
+    const handleClick = () => {
+        console.log('clicked');
+    }
     return(
-        <h1>barinda system sylvere</h1>
+        <>
+        <h1>{count}</h1>
+         <button onClick={handleClick} />
+         <button onClick={() => setCount(count + 1)}>click</button>
+        </>
     )
 }
