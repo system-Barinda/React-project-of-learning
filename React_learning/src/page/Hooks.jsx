d758f7a490a256ffea7188cdc7fe7d8d9b195cfa
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 10:57:49 +0200

    done

--- React_learning/src/page/Hooks.jsx
+++ React_learning/src/page/Hooks.jsx
@@ -216,50 +216,52 @@ import React, { useState, useCallback, useReducer, useEffect } from "react";
 //     )
 // }
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'incremented_age': {
-      return {
-        name: state.name,
-        age: state.age + 1
-      };
-    }
-    case 'changed_name': {
-      return {
-        name: action.nextName,
-        age: state.age
-      };
-    }
-  }
-  throw Error('Unknown action: ' + action.type);
-}
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'incremented_age': {
+//       return {
+//         name: state.name,
+//         age: state.age + 1
+//       };
+//     }
+//     case 'changed_name': {
+//       return {
+//         name: action.nextName,
+//         age: state.age
+//       };
+//     }
+//   }
+//   throw Error('Unknown action: ' + action.type);
+// }
 
-const initialState = { name: 'Taylor', age: 42 };
+// const initialState = { name: 'Taylor', age: 42 };
 
-export default function Form() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+// export default function Form() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
 
-  function handleButtonClick() {
-    dispatch({ type: 'incremented_age' });
-  }
+//   function handleButtonClick() {
+//     dispatch({ type: 'incremented_age' });
+//   }
 
-  function handleInputChange(e) {
-    dispatch({
-      type: 'changed_name',
-      nextName: e.target.value
-    }); 
-  }
+//   function handleInputChange(e) {
+//     dispatch({
+//       type: 'changed_name',
+//       nextName: e.target.value
+//     }); 
+//   }
 
-  return (
-    <>
-      <input
-        value={state.name}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleButtonClick}>
-        Increment age
-      </button>
-      <p>Hello, {state.name}. You are {state.age}.</p>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <input
+//         value={state.name}
+//         onChange={handleInputChange}
+//       />
+//       <button onClick={handleButtonClick}>
+//         Increment age
+//       </button>
+//       <p>Hello, {state.name}. You are {state.age}.</p>
+//     </>
+//   );
+// }
+
+
