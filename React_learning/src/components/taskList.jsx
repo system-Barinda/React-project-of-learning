046005b8e1a50af50c9f93dfc61019bfcce0931d
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 11:18:15 +0200

    done

--- React_learning/src/components/taskList.jsx
+++ React_learning/src/components/taskList.jsx
@@ -1,18 +1,15 @@
-// const tasks = [
-//      { id: 0, text: 'Visit Kafka Museum', done: true },
-//   { id: 1, text: 'Watch a puppet show', done: false },
-//   { id: 2, text: 'Lennon Wall pic', done: false }
-// ]
-
-export default function TaskList(tasks){
-    const taskLists = tasks.map((list) => {
-        return(<ol>
-            <li key={list.id}>{list.name}</li>
-        </ol>);
-    })
+export default function TaskList({ tasks }) {
+  const taskLists = tasks.map((list) => {
     return (
-        <div className="border">
-         
-        </div>
-    )
+      <li key={list.id}>{list.name}</li>
+    );
+  });
+
+  return (
+    <div className="border">
+      <ul>
+        {taskLists}
+      </ul>
+    </div>
+  );
 }
