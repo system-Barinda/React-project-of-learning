58e5ba69c5a1d35e41b9e2382a3fa0a411d95a52
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 12:20:06 +0200

    done

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App(){
   return (
     <>
-     <B/>
+     <LightSwitch/>
     </>
   );
 }
--- React_learning/src/page/todolist.jsx
+++ React_learning/src/page/todolist.jsx
@@ -1,31 +1,57 @@
-import TaskList from "../components/taskList"
-import {initialTasks} from "../data/initialTasks"
-import { useReducer } from "react";
-let nextId = 0;
+import TaskList from "../components/taskList";
+import { initialTasks } from "../data/initialTasks";
+import { useReducer, useState } from "react";
 
+let nextId = initialTasks.length;
 
+const reducerTask = (tasks, action) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
 
-const reducerTask = (tasks,action) => {
-  switch(action.type){
-    case 'add' : return { id :nextId++,text:nextName,done:true};
-    case 'change' : return { id :nextId--,text:nextName,done:true};
-    default : return {id,text,done};
+    case "change":
+      return tasks.map(task =>
+        task.id === action.id
+          ? { ...task, text: action.text, done: action.done }
+          : task
+      );
+
+    default:
+      return tasks;
+  }
+};
+
+export default function TodoList() {
+  const [tasks, dispatch] = useReducer(reducerTask, initialTasks);
+  const [text, setText] = useState("");
+
+  const handleAddTask = () => {
+    dispatch({ type: "add", id: nextId++, text });
+    setText("");
   };
-}
-export default function toDolist(){
-const [tasks,dispatch] = useReducer(reducerTask,initialTasks);
 
-const handleAddTask = () => {
-    dispatch({type:'add',id:nextId++,text:text})
-}
+  return (
+    <div className="mx-auto border h-100 w-200 shadow">
+      <input
+        type="text"
+        className="border"
+        name="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        placeholder="enter name"
+      />
+      <button className="border" onClick={handleAddTask}>
+        add
+      </button>
 
-
-    return(
-        <div className="mx-auto border h-100 w-200 shadow">
-       
-       <TaskList tasks={tasks} />
-
-        </div>
-        
-    )
+      <TaskList tasks={tasks} />
+    </div>
+  );
 }
