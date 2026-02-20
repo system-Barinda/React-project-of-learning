06a4053c69d6ac165cddb89b35c414bf73313c34
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 11:10:03 +0200

    done

--- React_learning/src/components/taskList.jsx
+++ React_learning/src/components/taskList.jsx
@@ -1,5 +1,18 @@
-const tasks = [
-     { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false }
-]
+// const tasks = [
+//      { id: 0, text: 'Visit Kafka Museum', done: true },
+//   { id: 1, text: 'Watch a puppet show', done: false },
+//   { id: 2, text: 'Lennon Wall pic', done: false }
+// ]
+
+export default function TaskList(tasks){
+    const taskLists = tasks.map((list) => {
+        return(<ol>
+            <li key={list.id}>{list.name}</li>
+        </ol>);
+    })
+    return (
+        <div className="border">
+         
+        </div>
+    )
+}
--- React_learning/src/page/todolist.jsx
+++ React_learning/src/page/todolist.jsx
@@ -1,7 +1,14 @@
+import TaskList from "../components/taskList"
+const tasks = [
+     { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
+]
 export default function toDolist(){
+    
     return(
         <div className="mx-auto border h-100 w-200 shadow">
-
+       <TaskList tasks= {tasks} />
         </div>
     )
 }
