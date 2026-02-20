b3e21da6dd32523ed592dfdf5f5190895e2cb9bf
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 11:03:32 +0200

    done"

---
+++ React_learning/src/components/taskList.jsx
@@ -0,0 +1,5 @@
+const tasks = [
+     { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
+]
