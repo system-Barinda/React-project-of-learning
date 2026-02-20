5cff67013f8b77f56867399825e9ddd4de806598
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 11:42:49 +0200

    done

--- React_learning/src/page/todolist.jsx
+++ React_learning/src/page/todolist.jsx
@@ -2,11 +2,7 @@ import TaskList from "../components/taskList"
 // import AddTask from "../components/AddTask"
 import { useReducer } from "react";
 let nextId = 0;
-const initialTasks = [
-     { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false }
-];
+
 
 
 const reducerTask = () => {
@@ -14,14 +10,15 @@ const reducerTask = () => {
 }
 export default function toDolist(){
 const [tasks,dispatch] = useReducer(reducerTask,initialTasks);
+
 const handleAddTask = () => {
     dispatch({type:'add',id:nextId++,text:text})
 }
+
+
     return(
         <div className="mx-auto border h-100 w-200 shadow">
-            {/* <AddTask
-        onAddTask={handleAddTask}
-      /> */}
+       
        <TaskList tasks={tasks} />
         </div>
     )
