196209c59ac9e102ea7c9337e036e7c3b3326de9
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-20 12:10:22 +0200

    done

--- React_learning/src/page/todolist.jsx
+++ React_learning/src/page/todolist.jsx
@@ -5,8 +5,12 @@ let nextId = 0;
 
 
 
-const reducerTask = () => {
-
+const reducerTask = (tasks,action) => {
+  switch(action.type){
+    case 'add' : return { id :nextId++,text:nextName,done:true};
+    case 'change' : return { id :nextId--,text:nextName,done:true};
+    default : return {id,text,done};
+  };
 }
 export default function toDolist(){
 const [tasks,dispatch] = useReducer(reducerTask,initialTasks);
@@ -20,7 +24,7 @@ const handleAddTask = () => {
         <div className="mx-auto border h-100 w-200 shadow">
        
        <TaskList tasks={tasks} />
-       
+
         </div>
         
     )
