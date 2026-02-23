d130789f9e97cb99f1dd145abc208757efba8c05
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 13:05:31 +0200

    done to create the useContext

--- React_learning/src/page/userContext.jsx
+++ React_learning/src/page/userContext.jsx
@@ -28,13 +28,63 @@
 //     </header>
 //   );
 // }
-import usercontext from "../components/componetUserContext"
-import DashBoardUseContext from "../components/DashboardUseContext";
-export default function MyComponent(){
-    const menu = ['home','service','contact','about us'];
-    return(<>
-      <usercontext.Provider value={menu}>
-        <DashBoardUseContext />
-      </usercontext.Provider>
-    </>)
+// import usercontext from "../components/componetUserContext"
+// import DashBoardUseContext from "../components/DashboardUseContext";
+// export default function MyComponent(){
+//     const menu = ['home','service','contact','about us'];
+//     return(<>
+//       <usercontext.Provider value={menu}>
+//         <DashBoardUseContext />
+//       </usercontext.Provider>
+//     </>)
+// }
+
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext('light');
+
+export default function MyApp() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <>
+      <ThemeContext value={theme}>
+        <Form />
+      </ThemeContext>
+      <Button onClick={() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+      }}>
+        Toggle theme
+      </Button>
+    </>
+  )
+}
+
+function Form({ children }) {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+
+function Button({ children, onClick }) {
+  const theme = useContext(ThemeContext);
+  const className = 'button-' + theme;
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
