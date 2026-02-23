edda742dc2d37135a64ce453a808549c4910a460
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 12:23:20 +0200

    done

--- React_learning/src/page/userContext.jsx
+++ React_learning/src/page/userContext.jsx
@@ -1,5 +1,22 @@
 import { createContext,useContext } from "react";
 const UserContext = createContext(null);
 export default function mycontenxt(){
+const user = {
+    name:"system sylvere",
+    role:"admin"
+};
+return(
+    <UserContext.Provider  value={user}>
+        <Dashboard />
+    </UserContext.Provider>
+)
+}
 
+function Dashboard(){
+    return(<header className="flex gap-1">
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+    </header>)
 }
