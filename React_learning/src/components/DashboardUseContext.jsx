bcc4bf38e39ba4dbca31cd3b9f0834e1e47ae50e
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 12:39:47 +0200

    chore:all related to the useContext

--- React_learning/src/components/DashboardUseContext.jsx
+++ React_learning/src/components/DashboardUseContext.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+import usercontext from "./componetUserContext"
 export default function DashBoardUseContext(){
+    const user = useContext(usercontext)
+    const menu = user.map((user) => {
+        return(
+            <header key={user} className="flex gap-1 bg-amber-400 text-white">
+                <div className="m-5 border rounded ">{user}</div>
+            </header>
+        )
+    })
     return(
         <>
-        
+          {menu}
         </>
     )
 }
