aec60f8a36c0b3423e7adb685dc6a96b96e0f60e
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 10:16:37 +0200

    chore:update all

--- React_learning/src/page/practise.jsx
+++ React_learning/src/page/practise.jsx
@@ -7,19 +7,27 @@ export default function Barinda(){
     const ingredi = items.map((item) => {
         return<li key={item}>{item}</li>
     })
+
+    const handleSubmitt = (event) => {
+   event.preventDefault();
+   const dataForm = new FormData(event.currentTarget);
+   let newItem = dataForm.getData("item");
+   setItemes(prev => [...prev,newItem]);
+    }
    
     return(
         <>
              <div className="flex ">
+                <form action="" onSubmit={handleSubmitt}>
                 <input type="text"
                 name="item"
                 placeholder="enter the ingradient"
                 className="border h-10 w-50 mr-5" />
-                <button className="border"> add items</button>
+                <button className="border" type="submit"> add items</button></form>
              </div>
 
              <div className="result">
-
+              {ingredi}
              </div>
         </>
     )
