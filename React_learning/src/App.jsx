4834293f4c2cd9ccb749d9474263f28b8fa72f13
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 12:31:45 +0200

    chore done to create the createContext

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import LightSwitch from "./page/practise"
+import LightSwitch from "./page/userContext"
 
 
 import './App.css'
--- React_learning/src/page/userContext.jsx
+++ React_learning/src/page/userContext.jsx
@@ -1,21 +1,36 @@
-import { createContext,useContext } from "react";
-const UserContext = createContext(null);
-export default function mycontenxt(){
-const user = {
-    name:"system sylvere",
-    role:"admin"
-};
-return(
-    <UserContext.Provider  value={user}>
-        <Dashboard />
-    </UserContext.Provider>
-)
-}
+// import { createContext, useContext } from "react";
 
-function Dashboard(){
-    const user = useContext(UserContext);
-    return(<header className="flex gap-1">
-         <h2>Welcome {user.name}</h2>
-      <p>Role: {user.role}</p>
-    </header>)
+// // create context
+// export const UserContext = createContext(null);
+
+// // Provider component
+// export default function MyContext() {
+//   const user = {
+//     name: "system sylvere",
+//     role: "admin",
+//   };
+
+//   return (
+//     <UserContext.Provider value={user}>
+//       <Dashboard />
+//     </UserContext.Provider>
+//   );
+// }
+
+// // Consumer component
+// function Dashboard() {
+//   const user = useContext(UserContext);
+
+//   return (
+//     <header className="flex gap-1">
+//       <h2>Welcome {user.name}</h2>
+//       <p>Role: {user.role}</p>
+//     </header>
+//   );
+// }
+
+export default function MyComponent(){
+    return(<>
+    <h1>barinda system system sylver</h1>
+    </>)
 }
