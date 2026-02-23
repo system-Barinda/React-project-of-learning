c69e27aa8665125432c9e7695e450ec6a5840565
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 10:32:53 +0200

    chore

--- React_learning/src/page/practise.jsx
+++ React_learning/src/page/practise.jsx
@@ -1,34 +1,60 @@
 import { useState } from "react";
 
-export default function Barinda(){
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"];
-    const[items,setItemes] = useState(ingredients);
+// export default function Barinda(){
+//     const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+//     const[items,setItemes] = useState(ingredients);
 
-    const ingredi = items.map((item) => {
-        return<li key={item}>{item}</li>
-    })
+//     const ingredi = items.map((item) => {
+//         return<li key={item}>{item}</li>
+//     })
 
-    const handleSubmitt = (event) => {
-   event.preventDefault();
-   const dataForm = new FormData(event.currentTarget);
-   let newItem = dataForm.get("item");
-   setItemes(prev => [...prev,newItem]);
-    }
+//     const handleSubmitt = (event) => {
+//    event.preventDefault();
+//    const dataForm = new FormData(event.currentTarget);
+//    let newItem = dataForm.get("item");
+//    setItemes(prev => [...prev,newItem]);
+//     }
    
-    return(
-        <>
-             <div className="flex ">
-                <form action="" onSubmit={handleSubmitt}>
-                <input type="text"
-                name="item"
-                placeholder="enter the ingradient"
-                className="border h-10 w-50 mr-5" />
-                <button className="border" type="submit"> add items</button></form>
-             </div>
+//     return(
+//         <>
+//              <div className="flex ">
+//                 <form action="" onSubmit={handleSubmitt}>
+//                 <input type="text"
+//                 name="item"
+//                 placeholder="enter the ingradient"
+//                 className="border h-10 w-50 mr-5" />
+//                 <button className="border" type="submit"> add items</button></form>
+//              </div>
 
-             <div className="result">
-              {ingredi}
-             </div>
-        </>
-    )
+//              <div className="result">
+//               {ingredi}
+//              </div>
+//         </>
+//     )
+// }
+export default function Barinda(){
+ const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (212) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    });
+
+    const allCantact = contact.map((user) => {
+        return(
+            <div className="h-100 w-100 border shadow-2xl rounded-2xl">
+                     <h1>{user.firstName}</h1>
+                     <h1>{user.lastName}</h1>
+                     <p>{user.phone}</p>
+                     <strong>{user.email}</strong>
+                     <strong>{user.isFavorite }</strong>
+            </div>
+        )
+    })
+return(
+    <>
+     {allCantact}
+    </>
+)
 }
