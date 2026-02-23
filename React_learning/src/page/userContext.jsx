4915d6bf4506979f2f047980a9eaa0e332271a0b
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 13:07:09 +0200

    done to update all done

--- React_learning/src/page/userContext.jsx
+++ React_learning/src/page/userContext.jsx
@@ -39,27 +39,31 @@
 //     </>)
 // }
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
-const ThemeContext = createContext('light');
+const ThemeContext = createContext("light");
 
 export default function MyApp() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
+
   return (
     <>
-      <ThemeContext value={theme}>
+      <ThemeContext.Provider value={theme}>
         <Form />
-      </ThemeContext>
-      <Button onClick={() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
-      }}>
+      </ThemeContext.Provider>
+
+      <Button
+        onClick={() =>
+          setTheme(theme === "dark" ? "light" : "dark")
+        }
+      >
         Toggle theme
       </Button>
     </>
-  )
+  );
 }
 
-function Form({ children }) {
+function Form() {
   return (
     <Panel title="Welcome">
       <Button>Sign up</Button>
@@ -70,18 +74,20 @@ function Form({ children }) {
 
 function Panel({ title, children }) {
   const theme = useContext(ThemeContext);
-  const className = 'panel-' + theme;
+  const className = "panel-" + theme;
+
   return (
     <section className={className}>
       <h1>{title}</h1>
       {children}
     </section>
-  )
+  );
 }
 
 function Button({ children, onClick }) {
   const theme = useContext(ThemeContext);
-  const className = 'button-' + theme;
+  const className = "button-" + theme;
+
   return (
     <button className={className} onClick={onClick}>
       {children}
