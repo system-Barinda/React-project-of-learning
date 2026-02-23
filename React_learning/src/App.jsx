21f6ec9d792a349ca4b6ea1783c5d690d6742783
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-23 10:12:39 +0200

    done to create the ingriedent item to be readable

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import LightSwitch from "./page/useMemos"
+import LightSwitch from "./page/practise"
 
 
 import './App.css'
--- React_learning/src/page/practise.jsx
+++ React_learning/src/page/practise.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Barinda(){
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+    const[items,setItemes] = useState(ingredients);
+
+    const ingredi = items.map((item) => {
+        return<li key={item}>{item}</li>
+    })
+   
     return(
         <>
              <div className="flex ">
                 <input type="text"
                 name="item"
-                placeholder="enter the ingradient" />
-                <button> add items</button>
+                placeholder="enter the ingradient"
+                className="border h-10 w-50 mr-5" />
+                <button className="border"> add items</button>
+             </div>
+
+             <div className="result">
+
              </div>
         </>
     )
