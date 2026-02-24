cc2cff43225f0b7ee7863635fd019bd3d0611a02
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-24 17:44:11 +0200

    chore:done

--- byWebpack/src/index.js
+++ byWebpack/src/index.js
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
-console.log(container)
 const root = createRoot(container);
 
 root.render(<App />);
-console.log(document.getElementById("root"));
-
-
