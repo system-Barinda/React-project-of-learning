43835eadce19f5b7fc431537ffded263796d8638
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-24 17:32:37 +0200

    chore:done to update all file"

--- byWebpack/src/index.js
+++ byWebpack/src/index.js
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
+console.log(container)
 const root = createRoot(container);
 
 root.render(<App />);
+console.log(document.getElementById("root"));
+
+
