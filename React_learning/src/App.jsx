14ea5da1166d8c6334394562bd1d54a84d7ee887
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-25 10:12:21 +0200

    chore:done

--- React_learning/src/App.jsx
+++ React_learning/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import LightSwitch from "./page/userContext"
+import LightSwitch from "./page/scrimber1"
 
 
 import './App.css'
--- React_learning/src/page/scrimber1.jsx
+++ React_learning/src/page/scrimber1.jsx
@@ -3,14 +3,11 @@ import React from "react"
 export default function App() {
     const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"])
     
-    /**
-     * Challenge:
-     * Only display the <h1> below if there are unread messages
-     */
+   
     
     return (
         <div>
-            <h1>You have _ unread messages!</h1>
+            <h1>{unreadMessages}</h1>
         </div>
     )
 }
