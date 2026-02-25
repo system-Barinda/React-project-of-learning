1a827b2eed3c9d0b1ae84c580d2fece08a35d678
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-25 17:17:58 +0200

    chore:done to update all version heads

--- React_learning/src/page/Form3.jsx
+++ React_learning/src/page/Form3.jsx
@@ -1,30 +1,56 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Form3(){
-    const[fname,setFname] = useState("");
-    const[lname,setLname] = useState("");
-    const handleSubmit = () => {
+export default function Form3() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
 
-        const handleFirst = (e) => {
-         setFname(e.target.value);
-        }
-         const handleLast = (e) => {
-         setLname(e.target.value);
-        }
-    }
+  const handleFirst = (e) => {
+    setFname(e.target.value);
+  };
 
-    return( <>
-        <form method="get" className="shadow bg-gray-200 mx-auto h-60 w-100" onSubmit={handleSubmit}>
-            <input type="text" onChange={handleFirst} placeholder="enter the first name" className="m-5 border-slate-300 border h-10 w-[90%]" /> <br />
-            <input type="text" onChange={handleLast} placeholder="enter the last name" className="m-5 border-slate-300 border h-10 w-[90%]" /> <br />
-            <input type="submit" value="send data" className="m-5 border h-10" />
-        </form>
+  const handleLast = (e) => {
+    setLname(e.target.value);
+  };
 
-        <div>
-            the first name: {fname} <br />
-            the last name: {lname}
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stop page reload
+    console.log("Submitted:", fname, lname);
+  };
+
+  return (
+    <>
+      <form
+        className="shadow bg-gray-200 mx-auto h-60 w-100"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          onChange={handleFirst}
+          placeholder="enter the first name"
+          className="m-5 border-slate-300 border h-10 w-[90%]"
+        />
+        <br />
+
+        <input
+          type="text"
+          onChange={handleLast}
+          placeholder="enter the last name"
+          className="m-5 border-slate-300 border h-10 w-[90%]"
+        />
+        <br />
+
+        <input
+          type="submit"
+          value="send data"
+          className="m-5 border h-10"
+        />
+      </form>
+
+      <div>
+        the first name: {fname} <br />
+        the last name: {lname}
+      </div>
     </>
-    )
+  );
 }
 
