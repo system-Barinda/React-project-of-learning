7dd60be6b4fe76ab2359ac4842ea48fe5c549323
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-25 10:30:34 +0200

    chore:done to update

--- React_learning/src/page/scrimber1.jsx
+++ React_learning/src/page/scrimber1.jsx
@@ -1,13 +1,27 @@
+// import React from "react"
+// export default function App() {
+//     const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"])
+//     return (
+//         <div>
+//             <h1>{unreadMessages}</h1>
+//         </div>
+//     )
+// }
+
 import React from "react"
 
 export default function App() {
-    const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"])
+    const [unreadMessages, setUnreadMessages] = React.useState(true)
     
-   
+     function toggleBtn(){
+        if(unreadMessages === true) {setUnreadMessages(false)}
+    else {setUnreadMessages(true)}
+        
+     }
     
     return (
         <div>
-            <h1>{unreadMessages}</h1>
+          <button className="border" onClick={toggleBtn}>{unreadMessages ? "hide" : "show"}panchline</button>
         </div>
     )
 }
