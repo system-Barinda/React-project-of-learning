bc9ba5c8f3f5fa69ecddf32fc0cfd2dc822b11b3
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-25 12:35:40 +0200

    chore:done to update all

--- React_learning/src/page/SaveButton.jsx
+++ React_learning/src/page/SaveButton.jsx
@@ -1,36 +1,56 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
-export default function SaveButton(){
-    const[isOnline,setIsOnline] = useState(true)
+// export default function SaveButton(){
+//     const[isOnline,setIsOnline] = useState(true)
 
-    useEffect(() => {
-        function handleOnline(){
-            setIsOnline(true);
-        }
-        function handleOffline(){
-            setIsOnline(false);
-        }
+//     useEffect(() => {
+//         function handleOnline(){
+//             setIsOnline(true);
+//         }
+//         function handleOffline(){
+//             setIsOnline(false);
+//         }
 
-        window.addEventListener('online',handleOnline);
-        window.addEventListener('offline',handleOffline);
+//         window.addEventListener('online',handleOnline);
+//         window.addEventListener('offline',handleOffline);
 
-        return () => {
-            window.removeEventListener('online',handleOnline);
-            window.removeEventListener('offline',handleOffline);
-        }
+//         return () => {
+//             window.removeEventListener('online',handleOnline);
+//             window.removeEventListener('offline',handleOffline);
+//         }
 
-    },[]);
+//     },[]);
 
-    const handleBTN = () => {
-      if(isOnline === true){
-        setIsOnline(false);
-      }
-      else{
-        setIsOnline(true)
-      }
+//     const handleBTN = () => {
+//       if(isOnline === true){
+//         setIsOnline(false);
+//       }
+//       else{
+//         setIsOnline(true)
+//       }
+//     }
+
+//     return(
+//         <button onClick={handleBTN} className="border h-20 w-30 mx-auto">{isOnline ? '👍 online' : '❌ Desconnected'}</button>
+//     )
+// }
+
+function statusBar(){
+    const isOnline = useOnlineStatus();
+    return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
+
+function SaveButtone(){
+    const isOnline = useOnlineStatus();
+
+    function handleSaveClick(){
+        console.log('✅ Progress saved');
     }
 
-    return(
-        <button onClick={handleBTN} className="border h-20 w-30 mx-auto">{isOnline ? '👍 online' : '❌ Desconnected'}</button>
-    )
+    return (
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? 'Save progress' : 'Reconnecting...'}
+    </button>
+  );
 }
+export default SaveButtone
