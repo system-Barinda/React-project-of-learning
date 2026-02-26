d818de80b59745a15e049406c5007cdf99aaf37b
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-26 15:17:13 +0200

    chore:done to update all files

--- React_learning/src/chefClaude/IngredientsList.jsx
+++ React_learning/src/chefClaude/IngredientsList.jsx
@@ -1,3 +1,15 @@
-export default function IngredientsList(){
-    return<h1>IngredientsList component</h1>
+export default function IngredientsList() {
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+            {ingredients.length > 3 && <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button onClick={toggleRecipeShown}>Get a recipe</button>
+            </div>}
+        </section>
+    )
 }
