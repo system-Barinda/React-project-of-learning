8ecac79cff882891e160ef611c9b95398c0d23d6
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-26 09:44:10 +0200

    chore:done to apply all

--- React_learning/src/page/GamePad.jsx
+++ React_learning/src/page/GamePad.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import pads from "./pads";
 
+export default function App() {
+  const [pad, setPad] = useState(pads);
 
-export default function App(){
-    const[pad,setPad] = useState(pads)
+  const allPads = pad.map((padItem) => {
+    return (
+      <button
+        key={padItem.id}
+        style={{ backgroundColor: padItem.color }}
+        className="m-2 p-4"
+      >
+        {padItem.text}
+      </button>
+    );
+  });
 
-    const allPad = pad.map((padd) => {
-        return(
-           <button key={padd.id} style={{backgroundColor:padd.color}}>{padd}</button>
-        )
-    })
-    
-    return(
-        <>
-         <div className="bg-green-300 text w-full h-full text-center mx-auto">
-              {allPad}
-            </div>
-        </>
-    )
+  return (
+    <div className="bg-green-300 w-full h-full text-center mx-auto">
+      {allPads}
+    </div>
+  );
 }
