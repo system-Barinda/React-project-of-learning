b977665e3e92124f0906e22f504dc55e9ed1d1de
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 16:34:02 +0200

    chore:done to learn

---
+++ React_learning/src/Pagination/CardUser.jsx
@@ -0,0 +1,8 @@
+export default function CardUser(){
+    return(
+        <div className="border h-80 w-80">
+          <p><strong>first name:</strong></p>
+          <p><strong>last name:</strong></p>
+        </div>
+    )
+}
--- React_learning/src/Pagination/UserPagnation.jsx
+++ React_learning/src/Pagination/UserPagnation.jsx
@@ -1,5 +1,11 @@
+import CardUser from "./CardUser"
 import users from "./users"
 export default function UserPagnation(){
    const data = users
    console.log(data)
+   return(
+    <>
+       <CardUser />
+    </>
+   )
 }
