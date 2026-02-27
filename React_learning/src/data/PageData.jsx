68289c77e9437f4cf416d4f16d025a8d06526d3e
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 10:48:08 +0200

    chore:done to run all

--- React_learning/src/data/PageData.jsx
+++ React_learning/src/data/PageData.jsx
@@ -1,19 +1,19 @@
 import { people } from "./data";
 import   th from "./th.jpg"
 
-export default function PageData (){
- const listItem = people.map(person => 
-    <li key={person.id}>
-        <img src={th(person)}
-             alt={person.name} />
+function getImageUrl(person) {
+  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
+}
 
-              <p>
-        <b>{person.name}</b>
-          {' ' + person.profession + ' '}
-          known for {person.accomplishment}
-      </p>
+export default function PageData (){
+  const listItem = people.map(person => 
+    <li key={person.id}>
+        {/* 3. Call the helper function here */}
+        <img src={getImageUrl(person)} alt={person.name} />
+        <p>
+          <b>{person.name}</b> known for {person.accomplishment}
+        </p>
     </li>
-    
- )
- return <ul>{listItem}</ul>
+  )
+  return <ul>{listItem}</ul>
 }
