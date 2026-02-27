07e1abc33d87501853505ed098e3b98a1e40a042
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 10:45:28 +0200

    done

--- React_learning/src/data/PageData.jsx
+++ React_learning/src/data/PageData.jsx
@@ -2,5 +2,18 @@ import { people } from "./data";
 import   th from "./th.jpg"
 
 export default function PageData (){
-    return(<img src={th} />)
+ const listItem = people.map(person => 
+    <li key={person.id}>
+        <img src={th(person)}
+             alt={person.name} />
+
+              <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
+    </li>
+    
+ )
+ return <ul>{listItem}</ul>
 }
