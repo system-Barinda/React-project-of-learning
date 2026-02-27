f2c40cf3ad53b621fe2bfe20e19ebf1f3904e8a7
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 10:52:42 +0200

    chore:done

--- React_learning/src/data/PageData.jsx
+++ React_learning/src/data/PageData.jsx
@@ -1,19 +1,26 @@
-import { people } from "./data";
-import   th from "./th.jpg"
+// import { people } from "./data";
+// import   th from "./th.jpg"
 
-function getImageUrl(person) {
-  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
-}
+// function getImageUrl(person) {
+//   return 'https://i.imgur.com/' + person.imageId + 's.jpg';
+// }
 
-export default function PageData (){
-  const listItem = people.map(person => 
-    <li key={person.id}>
-        {/* 3. Call the helper function here */}
-        <img src={getImageUrl(person)} alt={person.name} />
-        <p>
-          <b>{person.name}</b> known for {person.accomplishment}
-        </p>
-    </li>
-  )
-  return <ul>{listItem}</ul>
+// export default function PageData (){
+//   const listItem = people.map(person => 
+//     <li key={person.id}>
+//         {/* 3. Call the helper function here */}
+//         <img src={getImageUrl(person)} alt={person.name} />
+//         <p>
+//           <b>{person.name}</b> known for {person.accomplishment}
+//         </p>
+//     </li>
+//   )
+//   return <ul>{listItem}</ul>
+// }
+
+import { people} from "./data";
+import thee from "./th.jpg"
+
+function getImageUrl(person){
+    return 'http://i.imgur.com/' + person.imageId + 's.jpg'
 }
