1d36b6f30c1fa6d038f40271601f23dac262e1f2
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 12:42:24 +0200

    chore:done to update all

--- React_learning/src/Pagination/card.jsx
+++ React_learning/src/Pagination/card.jsx
@@ -1,15 +1,23 @@
-export default function Card(props){
-    const proje = props.data.map((user) => {
-       return(<li key={user.id}>{user.name}</li>)
-    })
-    return(
-        <div className="border h-80 w-70 ">
-            <div className="imgDiv border h-40 w-[90%] m-2"></div>
-            <div className="detailes border h-20 w-[90%] m-2">
-                <p>name:{data}</p>
-                <p>price:</p>
-                <button>buy</button>
-            </div>
-        </div>
-    )
+export default function Card({ data }) {
+    return (
+        <>
+            {data.map((product) => (
+                <div key={product.id} className="border h-80 w-70 m-4 p-2">
+                    <div className="imgDiv border h-40 w-[90%] m-2">
+                        <img 
+                            src={product.image} 
+                            alt={product.title} 
+                            className="h-full mx-auto"
+                        />
+                    </div>
+
+                    <div className="details border h-20 w-[90%] m-2">
+                        <p>Title: {product.title}</p>
+                        <p>Price: ${product.price}</p>
+                        <button>Buy</button>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 }
