11d689a777fecc135edbba7de6a827219748ea02
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 16:59:50 +0200

    chore:done to learn all

--- React_learning/src/Pagination/CardUser.jsx
+++ React_learning/src/Pagination/CardUser.jsx
@@ -1,17 +1,19 @@
-export default function CardUser({user}){
+export default function CardUser({ user }) {
     const listUser = user.map((person) => {
-        return(
-        <div key={person.id} className="border h-80 w-80">
-          <p><strong>first name:</strong></p>
-          <p><strong>last name:</strong></p>
-          <p><strong>username:</strong></p>
-          <p><strong>user email:</strong></p>
-        </div>
         
-        )
-    })
-    return(
-          {listUser}
-        
-    )
+        return (
+            <div key={person.id} className="border h-80 w-80 p-4 m-2">
+                <p><strong>First name:</strong> {person.name.firstname}</p>
+                <p><strong>Last name:</strong> {person.name.lastname}</p>
+                <p><strong>Username:</strong> {person.username}</p>
+                <p><strong>User email:</strong> {person.email}</p>
+            </div>
+        );
+    });
+
+    return (
+        <>
+            {listUser}
+        </>
+    );
 }
