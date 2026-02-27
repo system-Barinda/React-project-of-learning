8401fff5e5af7db9a8a41bafb3b2380670ec5da3
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 17:05:12 +0200

    chore:done to update all

--- React_learning/src/Pagination/UserPagnation.jsx
+++ React_learning/src/Pagination/UserPagnation.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 import CardUser from "./CardUser"
 import users from "./users"
 export default function UserPagnation(){
    const data = users
    
+   const[currentPage,setCurrentPage] = useState(1);
+   const itemParPage = 5;
+
+   const totalPage = Math.ceil(data.length / itemParPage);
+   const startIndex = (currentPage - 1) * itemParPage;
+   const endIndex =  startIndex + itemParPage;
+
+   currentItem = data.slice(startIndex,endIndex);
+   
    return(
     <>
-       <CardUser user={data}/>
+       <CardUser user={currentItem} />
     </>
    )
 }
