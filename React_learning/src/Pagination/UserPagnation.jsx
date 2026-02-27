81e4cd46f1a4f297d756d5463c0ec1128521eea5
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 16:29:02 +0200

    chore:done to run all

--- React_learning/src/Pagination/UserPagnation.jsx
+++ React_learning/src/Pagination/UserPagnation.jsx
@@ -1,9 +1,5 @@
 import users from "./users"
 export default function UserPagnation(){
-    console.log(users[0].name)
-    return(<ol>
-        {users.forEach((user) => (
-            <li key={user.id}>{user.name}</li>
-        ))}
-    </ol>)
+   const data = users
+   console.log(data)
 }
