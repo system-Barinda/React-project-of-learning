04da7273a7d900856bfe26d97882b36ce9f86cbb
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-27 17:16:37 +0200

    done to learn how to make pagnation

--- React_learning/src/Pagination/UserPagnation.jsx
+++ React_learning/src/Pagination/UserPagnation.jsx
@@ -12,7 +12,12 @@ export default function UserPagnation(){
    const endIndex =  startIndex + itemParPage;
 
    const currentItem = data.slice(startIndex,endIndex);
-
+const handleLeft = () => {
+    setCurrentPage(prev => prev - 1);
+}
+const handleRight = () => {
+    setCurrentPage(prev => prev + 1);
+}
    return(<>
     <div className="flex">
        <CardUser user={currentItem} />
@@ -20,10 +25,10 @@ export default function UserPagnation(){
        
     </div>
 
-    <div className="shadow-2xs h-20 w-80 mx-auto bg-gray-300 mt-10 flx items-center justify-center">
-     <button className="m-5 border h-10 w-10"> ⬅️</button>
-     <span className="border">page  of </span>
-     <button className="m-5 border h-10 w-10"> ➡️ </button>
+    <div className="shadow-2xs h-20 w-80 mx-auto bg-gray-300 mt-10 flx items-center justify-center text-center">
+     <button disabled={currentPage === 1} onClick={handleLeft} className="m-5 border h-10 w-10"> ⬅️</button>
+     <span className="border">page {currentPage}  of {totalPage} </span>
+     <button disabled={currentPage === totalPage} onClick={handleRight} className="m-5 border h-10 w-10"> ➡️ </button>
     </div>
    </>)
 }
