691dca2a489e148708dbf86fe46eb31234e44f25
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-02 10:50:48 +0200

    chore:done to learn

--- React_learning/src/props/App.jsx
+++ React_learning/src/props/App.jsx
@@ -10,14 +10,29 @@
 // }
 
 
+// import UserCard from "./UserCard"
+
+// export default function App(){
+//   return(
+//     <UserCard 
+//     name="barinda"
+//     age={20}
+//     location="muhanga"
+//     />
+//   )
+// }
+
 import UserCard from "./UserCard"
 
 export default function App(){
+  const data = {
+    name:'barinda system system',
+    age:21,
+    location:'muhanga'
+  }
   return(
     <UserCard 
-    name="barinda"
-    age={20}
-    location="muhanga"
+        data={data}
     />
   )
 }
--- React_learning/src/props/UserCard.jsx
+++ React_learning/src/props/UserCard.jsx
@@ -8,12 +8,21 @@
 // }
 
 
-export default function UserCard({name,age,location}){
-  const alldata = {name,age,location}
-  console.log(alldata)
+// export default function UserCard({name,age,location}){
+//   const alldata = {name,age,location}
+//   console.log(alldata)
+//   return(<>
+//     <h1>name :{name}</h1>
+//     <p>age:{age}</p>
+//     <strong>location:{location}</strong>
+//   </>)
+// }
+
+export default function UserCard({data}){
+  
   return(<>
-    <h1>name :{name}</h1>
-    <p>age:{age}</p>
-    <strong>location:{location}</strong>
+    <h1>name :{data.name}</h1>
+    <p>age:{data.age}</p>
+    <strong>location:{data.location}</strong>
   </>)
 }
