d7976570fb60f78e41c49e513ce69a0269cfd6c9
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-17 19:37:46 +0200

    chore done to update data

--- React_learning/src/page/UserPageLoading.jsx
+++ React_learning/src/page/UserPageLoading.jsx
@@ -1,7 +1,35 @@
 export default function UserPageLoading(){
-    return(
-        <h1>barinda</h1>
-    )
+
+    const [users, setUsers] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(null); 
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getAllData();
+        setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+    if (loading) return <UserPageLoading />;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className="App">
+      <h1>Users:</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 
