8464ee5634193a248bfec65599f89acc8d1b0f19
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-18 17:02:18 +0200

    done

--- lidax/src/features/Users.jsx
+++ lidax/src/features/Users.jsx
@@ -1,6 +1,12 @@
-export default function Users(){
-    return(
-        <>
-        </>
-    )
-}
+import {createSlice} from "@reduxjs/toolkit"
+
+export const userSlice = createSlice({
+    name:"user",
+    initialState:{value:{name:"", age:0, email:""}},
+    reducers:{
+        login:(state,action) =>{
+            state.value = action.payload
+        } 
+    }
+})
+export default userSlice.reducer 
