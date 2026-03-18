00c3e250a6c9d230c72f2ec5a7e86527340b5c38
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-18 16:53:17 +0200

    done

--- lidax/src/main.jsx
+++ lidax/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {configureStore} from "@reduxjs/toolkit"
+import {Provider} from "react-redux"
+
+const store = configureStore({
+  reducer:{}
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
