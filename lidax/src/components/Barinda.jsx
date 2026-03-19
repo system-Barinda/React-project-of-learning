58d6f720ac68dfd221b5d4a917f43cb21d2d7143
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-19 20:31:51 +0200

    chore:done

--- lidax/src/components/Barinda.jsx
+++ lidax/src/components/Barinda.jsx
@@ -2,13 +2,15 @@ import { useSelector } from "react-redux"
 
 export default function Barinda(){
     const userBarinda = useSelector((state) => state.user.value)
+    const changecolor = useSelector((state) => state.theme.value)
     return(
         <>
         <h1>the page of barinda</h1>
-        <div>
+        <div style={{background:changecolor}}>
          the name:{userBarinda.name}
          <br />
          <strong>email:{userBarinda.email}</strong>
+         
         </div>
         </>
     )
