f67c66ad347b11a46367e0dc14ba5da9cc271ad0
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 09:41:33 +0200

    chore:done to update all the data through the other file of control

---
+++ lidax/src/Operation/Control.jsx
@@ -0,0 +1,11 @@
+import ButtonLogin from "../pages/ButtonLogin";
+import UserPage from "../pages/userPage";
+
+export default function Control(){
+    return(
+        <>
+        <UserPage />
+        <ButtonLogin />
+        </>
+    )
+}
