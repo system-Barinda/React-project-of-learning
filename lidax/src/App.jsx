e9e5b8983c9b5738c8e1f3a9917dcfb186e49f98
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 10:01:12 +0200

    chore:done to update

--- lidax/src/App.jsx
+++ lidax/src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Control from './Operation/Control'
-import {configureStore} from "@reduxjs/toolkit"
-import {Provider} from "react-redux"
-import userSlicer  from "./features/Peaple"
+import { useState } from 'react';
+import './App.css';
+import Control from './Operation/Control';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "./features/Peaple";
+
 export const store = configureStore({
-  reducer:{
-    reducers:userSlicer
+  reducer: {
+    user: userReducer   
   }
-})
+});
 
 function App() {
-  
   return (
-  
     <Provider store={store}>
-     <Control />
-     </Provider>
-     
-    
-  )
+      <Control />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
