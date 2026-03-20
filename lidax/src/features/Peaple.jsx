893fae37b2459f036e848d9dfe5afe0aa4f12790
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 09:59:54 +0200

    chore:done to update

--- lidax/src/features/Peaple.jsx
+++ lidax/src/features/Peaple.jsx
@@ -1,14 +1,23 @@
-import { createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const userSlicer = createSlice({
-     name:"userbarinda",
-     initialState:{value:{name:"",age:0,email:""}},
-     reducers:{
-        Login:(state,action) => { state.value = action.payload},
-        Logout:(state) => { state.value = {name:"",age:0,email:""} }
-     }
-     
-})
+const initialState = {
+  name: "",
+  age: 0,
+  email: ""
+};
 
-export const { Login} = userSlicer.actions;
-export default userSlicer.reducer;
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      return action.payload;
+    },
+    logout: () => {
+      return initialState;
+    }
+  }
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
