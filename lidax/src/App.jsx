67b50974aa711eb487779cd36de2a432df9bb12a
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 09:34:54 +0200

    chore:doen t done to update and make practice

--- lidax/src/App.jsx
+++ lidax/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import Barinda from './components/Barinda'
 import './App.css'
-import BarindaLogin from './components/BarindaLogin'
-import ChangeColor from './components/Changecolor'
+
 
 function App() {
   
   return (
     <>
-      <Barinda />
-      <BarindaLogin />
-      <ChangeColor />
+     
      
     </>
   )
--- lidax/src/main.jsx
+++ lidax/src/main.jsx
@@ -3,22 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import {configureStore} from "@reduxjs/toolkit"
-import {Provider} from "react-redux"
-import UserReducer from "./features/BarindaUsers.jsx"
-import themeReducer from "./features/theme.jsx"
 
-const store = configureStore({
-  reducer:{
-    user:UserReducer,
-    theme:themeReducer
-  }
-});
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+   
     <App />
-    </Provider>
+   
   </StrictMode>,
 )
