c514f9d634e5d03d8dee51f70debb2240f4b4687
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 09:37:24 +0200

    chore:done"

--- lidax/src/App.jsx
+++ lidax/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import UserPage from './pages/userPage'
 
 function App() {
   
   return (
     <>
-     
+     <UserPage />
      
     </>
   )
---
+++ lidax/src/pages/userPage.jsx
@@ -0,0 +1,7 @@
+export default function UserPage(){
+    return(
+        <div>
+            <h1>the page of user data</h1>
+        </div>
+    )
+}
