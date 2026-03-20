bac438e3c6080b9d53264395d696f2c91812f3df
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-20 10:02:24 +0200

    chore:done  update all thing to work well

--- lidax/src/pages/userPage.jsx
+++ lidax/src/pages/userPage.jsx
@@ -1,13 +1,14 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 
-export default function UserPage(){
-    const stateData = useSelector((state) => state.userbarinda.value);
+export default function UserPage() {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    return(
-        <div>
-            <h1>the page of user data</h1> <br />
-            <strong>the name:{stateData.name}</strong>
-        </div>
-    )
+  return (
+    <div>
+      <h1>The page of user data</h1>
+      <br />
+      <strong>The name: {user.name}</strong>
+    </div>
+  );
 }
