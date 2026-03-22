19e7d20d1755ba3a35514a495919b0f22e0eb8c7
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-22 19:23:09 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -4,6 +4,11 @@ function App(){
   return(
     <>
     <h1>barinda system sylvere</h1>
+
+    <div>
+      <button>add student</button>
+      <button>view student</button>
+    </div>
     <AddStudent />
     </>
   )
