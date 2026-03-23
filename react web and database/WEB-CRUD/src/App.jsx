ac9df71444024946249a2a9420b86e989e880c38
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-23 10:32:42 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,13 +1,29 @@
 function App(){
   const getMoney = "withdraws";
+
   const initialState = { AmounOfMoney:2000};
+
   function AccessWithdraws(){
     return {
       type:getMoney,
       info:"you have made withdraw seccefful",
     }
   }
-  
+
+const reducer = (state = initialState, actions) => {
+     switch(actions.type){
+      case getMoney :
+        return {
+          ...state,
+          AmounOfMoney:state.AmounOfMoney - 100,
+        };
+      default:
+        return state;  
+     }
+}
+
+
+
   return(
     <h1>barinda</h1>
   )
