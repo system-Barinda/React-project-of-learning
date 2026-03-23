72be42bde8186355b81c83c4842dc4b76e1ca120
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-23 11:35:02 +0200

    done

--- react web and database/WEB-CRUD/src/contextApi/ContextApi1.jsx
+++ react web and database/WEB-CRUD/src/contextApi/ContextApi1.jsx
@@ -1,9 +1,16 @@
-import { useContext } from "react"
+import { useContext } from "react";
+import { ContextApi } from "../App"; 
 
-export default function ContextApi1(){
-    const {data} = useContext(contextApi);
-    console.log(data)
-    return(
-        <h1>done</h1>
-    )
+export default function ContextApi1() {
+  const data = useContext(ContextApi); 
+
+  console.log(data);
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <h1>{data.location}</h1>
+      <h1>{data.country}</h1>
+    </div>
+  );
 }
