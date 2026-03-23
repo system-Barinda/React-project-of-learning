1433f157f9f75b725d3617d8d244dde4d6a7576a
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-23 09:32:05 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,4 +1,12 @@
 function App(){
+  const buy_cake = 'barinda system sylvere';
+   function barindaCake(){
+    return{
+      type:buy_cake,
+      type1:'done to update it',
+    };
+   }
+   console.log(barindaCake().type);
   return(
     <h1>barinda system sylvere</h1>
   )
