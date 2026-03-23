7900059adce860e0cf5e7a0c51a5462e10e17a6a
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-23 10:29:54 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,4 +1,13 @@
 function App(){
+  const getMoney = "withdraws";
+  const initialState = { AmounOfMoney:2000};
+  function AccessWithdraws(){
+    return {
+      type:getMoney,
+      info:"you have made withdraw seccefful",
+    }
+  }
+  
   return(
     <h1>barinda</h1>
   )
