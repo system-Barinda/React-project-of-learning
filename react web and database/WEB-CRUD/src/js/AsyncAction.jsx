b289c6ab6b07c061e0d4d89d69ad4cd3639e1776
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-24 11:34:42 +0200

    done

--- react web and database/WEB-CRUD/src/js/AsyncAction.jsx
+++ react web and database/WEB-CRUD/src/js/AsyncAction.jsx
@@ -1,37 +1,41 @@
+import { createStore } from "redux"; 
+
 const initialState = {
-    loading:false,
-    users:[data],
-    error:''
+    loading: false,
+    users: [], // Changed from [data] to empty array
+    error: ''
 };
 
-const   fetch_users_request = 'fetch_users_request'
-const   fetch_users_success = 'fetch_users_success'
-const   fetch_users_failure = 'fetch_users_failure'
+// 2. Action Constants
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+// 3. Action Creators
+const fetchUsersRequest = () => ({ type: FETCH_USERS_REQUEST });
+
+const fetchUsersSuccess = users => ({
+    type: FETCH_USERS_SUCCESS,
+    payload: users 
+});
+
+const fetchUserFailure = error => ({
+    type: FETCH_USERS_FAILURE,
+    payload: error
+});
 
 
-const fetchUsersRequest = () => {
-    return{
-        type:fetch_users_request,
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_USERS_REQUEST: 
+            return { ...state, loading: true };
+        case FETCH_USERS_SUCCESS: 
+            return { loading: false, users: action.payload, error: '' };
+        case FETCH_USERS_FAILURE: 
+            return { loading: false, users: [], error: action.payload };
+        default: 
+            return state; 
     }
 }
 
-const fetchUsersSuccess = users => {
-    return {
-        type:fetch_users_success,
-        payLoad:users,
-    }
-}
-
-const fetchUserFailure = error => {
-    return{
-        type:fetch_users_failure,
-        payLoad:error
-    }
-}
-const reducer = (state,action) => {
-    switch(action.type){
-        case fetch_users_request : return {...state,loading:true}
-        case fetch_users_success : return {loading:false,users:action.payLoad,error:''}
-        case fetch_users_failure : return {loading:false,users:[],error:action.payLoad}
-    }
-}
+const store = createStore(reducer);
