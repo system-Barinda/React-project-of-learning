0ccd11ea527e9f1c081314a3422b35a6cafc3a40
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-24 10:26:07 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,4 +1,30 @@
+import {createStore} from "redux";
+
 function App(){
+  const Get_money = 'make withdraws';
+
+  function withdrow(){
+    return{
+      type:Get_money,
+      info:'the money you have is there'
+    }
+  }
+  const intialState = {
+    amount:2000,
+  };
+
+  const reducer = (state = intialState,actions) => {
+      switch(actions.type){
+        case Get_money : 
+        return {
+          ...state,
+          amount:state.amount - 100
+        }
+
+      }
+  }
+
+  
   return(
     <h1>barinda system sylvere</h1>
   )
