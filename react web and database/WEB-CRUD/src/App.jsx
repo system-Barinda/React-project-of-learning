c8005142e958cce7d2d977cd63835899f3a5c8c0
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-24 10:47:40 +0200

    done to update all

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,65 +1,74 @@
-import {combineReducers, createStore} from "redux";
+import { combineReducers, createStore } from "redux";
 
-function App(){
+function App() {
+  // 1. Define all action types
   const Get_money = 'make withdraws';
-  const Keep_money = 'keep money';
+  const Keep_money = 'keep money'; // Added missing definition
 
-  function withdrow(){
-    return{
-      type:Get_money,
-      info:'the money you have is there'
+  // 2. Action Creators
+  function withdrow() {
+    return {
+      type: Get_money,
+      info: 'the money you have is there'
     }
   }
 
-  function updateMoney(){
-    return{
-      type:Keep_money,
-      info:'you saved the money well'
+  function updateMoney() {
+    return {
+      type: Keep_money,
+      info: 'you saved the money well'
     }
   }
-  
+
   const intialState = {
-    amount:2000,
+    amount: 2000,
   };
 
-  const reducer = (state = intialState,actions) => {
-      switch(actions.type){
-        case Get_money : 
+  // 3. Reducers (Added default returns)
+  const reducer = (state = intialState, actions) => {
+    switch (actions.type) {
+      case Get_money:
         return {
           ...state,
-          amount:state.amount - 100
+          amount: state.amount - 100
         }
-        default:return state
-
-      }
+      default:
+        return state; // Crucial for Redux
+    }
   }
 
-    const reducerUpdate = (state = intialState,actions) => {
-      switch(actions.type){
-        case Keep_money : 
+  const reducerUpdate = (state = intialState, actions) => {
+    switch (actions.type) {
+      case Keep_money:
         return {
           ...state,
-          amount:state.amount + 1000
+          amount: state.amount + 1000
         }
-       default:return state
-      }
+      default:
+        return state; // Crucial for Redux
+    }
   }
 
-
-  const rootStore = combineReducers({
-    withDraw:reducer,
-    save:reducerUpdate,
+  // 4. Combine Reducers into a Root Reducer
+  const rootReducer = combineReducers({
+    withDraw: reducer,
+    save: reducerUpdate,
   });
-  const store = createStore(rootStore);
-  store.subscribe(() =>  console.lopg('initial money',state.getState()))
 
+  // 5. Create the Store using the rootReducer
+  const store = createStore(rootReducer);
+
+  // 6. Test it
+  console.log('Initial State:', store.getState());
   
-  store.dispatch(withdrow())
-  store.dispatch(updateMoney())
-  console.log('initial money',store.getState());
+  store.dispatch(withdrow());
+  store.dispatch(updateMoney());
+  
+  console.log('Updated State:', store.getState());
 
-  return(
+  return (
     <h1>barinda system sylvere</h1>
-  )
+  );
 }
+
 export default App;
