9422cd7dae5ddcf493b80b7f5c49a7c18dec3426
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-25 13:45:22 +0200

    done to update all

--- react web and database/server/controllers/authController.js
+++ react web and database/server/controllers/authController.js
@@ -12,3 +12,21 @@ export const register = async(req,res) => {
         res.json("user registered");
     });
 };
+
+export const login = async(req,res) => {
+    const {email,password} = req.body;
+    const sql = "SELECT * FROM users WHERE email = ?";
+    db.query(sql,[email,password],(error,result) => {
+      if(error) return res.status(500).json(error.message);
+      if(result.length === 0) return res.status(404).json("user not found");
+
+      const user = result[0];
+      const isMatch = bcrypt.compare(password,user.password);
+      if(!isMatch) return res.status(400).json('wrong password');
+      const token = jwt.sign({id:user.id},"secretkey",{
+        expiresIn:"1h",
+      });
+      res.json({token});
+
+    });
+};
