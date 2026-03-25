41c164c8366ad7b997ddce85ed52dab3275cbf5f
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-25 13:01:41 +0200

    done

--- react web and database/server/db.js
+++ react web and database/server/db.js
@@ -6,3 +6,9 @@ const db = mysql.createConnection({
     password:"",
     database:"auth_app"
 });
+
+db.connect((err) => {
+    if(err) console.log(err.message);
+    else console.log("mysql connected well");
+})
+export default db;
