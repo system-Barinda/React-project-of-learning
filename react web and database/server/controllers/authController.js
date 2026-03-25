95ac3637d0417a30c87b6e5c732e73c45b8f4c04
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-25 13:10:09 +0200

    done

--- react web and database/server/controllers/authController.js
+++ react web and database/server/controllers/authController.js
@@ -2,3 +2,13 @@ import db from "../db";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+export const register = async(req,res) => {
+    const {username,email,password} = req.body;
+    const hashedPassword = await bcrypt.hash(password,10);
+    const sql = "INSERT INTO users (username,email,password)value(?,?,?)";
+
+    db.query(sql,[username,email,hashedPassword],(error) => {
+        if(error) return res.status(500).json(error);
+        res.json("user registered");
+    });
+};
