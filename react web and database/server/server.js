bc0270f3c62ad2eabd23984acbd5ca1ccf3a8795
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-25 16:15:17 +0200

    done

--- react web and database/server/server.js
+++ react web and database/server/server.js
@@ -9,7 +9,12 @@ app.use(express.json());
 
 app.use("/api/auth",authRoutes);
 
-
+app.get("/api/protected",authMiddleware,(req,res) => {
+  res.json({
+    message:"You accessed protected data ";
+    user:req.user
+  });
+});
 
 app.listen(5000,() => {
   console.log("server running on port 5000");
