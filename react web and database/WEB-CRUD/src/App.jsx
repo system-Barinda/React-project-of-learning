dacdb3018132c73f7509907e501c9a19242fc004
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-25 16:50:57 +0200

    chore:done to udpate all

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
-function App(){
-  return(
-    <Dashboard />
-  )
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <>
+      {isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        <Login setIsLoggedIn={setIsLoggedIn} />
+      )}
+    </>
+  );
 }
-export default App
+
+export default App;
