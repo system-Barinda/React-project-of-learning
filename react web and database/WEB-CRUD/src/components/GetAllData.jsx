fe9e6fcaa3daba522373c4eec52a1889cffb5c1a
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 12:07:24 +0200

    done

--- react web and database/WEB-CRUD/src/components/GetAllData.jsx
+++ react web and database/WEB-CRUD/src/components/GetAllData.jsx
@@ -2,8 +2,18 @@ import { fetchuserfailure,fetchuserrequest,fetchusersuccess } from "../service/r
 export default function GetAllData(){
        function dataFetch(){
         return function(dispatch){
+            try{
+            dispatch(fetchuserrequest());
          const res = fetch('https://jsonplaceholder.typicode.com/users');
-        };
+         if(!res.ok) return dispatch(fetchuserfailure(new Error('failed to get all data')));
+         const data = res.json();
+         dispatch(fetchusersuccess(data));
+
+        }
+        catch(err){
+            dispatch(fetchuserfailure(new Error(err.message)));
+        }
+    };
        }
     return(<h1>done</h1>);
 }
