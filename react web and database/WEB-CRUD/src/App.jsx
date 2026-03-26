7cf5bcc500d6bcc377964db58c6a45eb37aac6f5
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 15:14:37 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,10 +1,12 @@
+import UserProvider from "./contextApi/UserContext";
 import User from "./contextApi/User";
 
-
-function App(){
-
-  return(
-  <User />
-  )
+function App() {
+  return (
+    <UserProvider>
+      <User />
+    </UserProvider>
+  );
 }
+
 export default App;
--- react web and database/WEB-CRUD/src/contextApi/User.jsx
+++ react web and database/WEB-CRUD/src/contextApi/User.jsx
@@ -1,8 +1,17 @@
-import { useContext } from "react"
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 
-export default function User(){
-    const user = useContext()
-    return(
-        <h1>the page of user</h1>
-    )
+export default function User() {
+  const { users, loading, error } = useContext(UserContext);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error: {error}</h2>;
+
+  return (
+    <div>
+      {users.map((u) => (
+        <p key={u.id}>{u.name}</p>
+      ))}
+    </div>
+  );
 }
