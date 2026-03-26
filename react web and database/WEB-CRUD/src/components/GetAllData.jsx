656b77c3d17cc9ff199e6ca67c01c3fd3a420e59
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 12:09:40 +0200

    done

--- react web and database/WEB-CRUD/src/components/GetAllData.jsx
+++ react web and database/WEB-CRUD/src/components/GetAllData.jsx
@@ -1,19 +1,28 @@
-import { fetchuserfailure,fetchuserrequest,fetchusersuccess } from "../service/reducer";
-export default function GetAllData(){
-       function dataFetch(){
-        return function(dispatch){
-            try{
-            dispatch(fetchuserrequest());
-         const res = fetch('https://jsonplaceholder.typicode.com/users');
-         if(!res.ok) return dispatch(fetchuserfailure(new Error('failed to get all data')));
-         const data = res.json();
-         dispatch(fetchusersuccess(data));
+import { fetchuserfailure, fetchuserrequest, fetchusersuccess } from "../service/reducer";
 
+export default function GetAllData() {
+
+  function dataFetch() {
+    return async function (dispatch) {
+      try {
+        dispatch(fetchuserrequest());
+
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if (!res.ok) {
+          return dispatch(fetchuserfailure('Failed to get all data'));
         }
-        catch(err){
-            dispatch(fetchuserfailure(new Error(err.message)));
-        }
+
+        const data = await res.json();
+
+        dispatch(fetchusersuccess(data));
+
+      } catch (err) {
+        dispatch(fetchuserfailure(err.message));
+      }
     };
-       }
-    return(<h1>done</h1>);
+  }
+  dataFetch();
+
+  return (<h1>done</h1>);
 }
