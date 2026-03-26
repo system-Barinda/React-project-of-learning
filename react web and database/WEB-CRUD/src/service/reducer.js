d786e95c404eac07d4dfddaa8466cfa8af7d0b71
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 12:52:10 +0200

    done

--- react web and database/WEB-CRUD/src/service/reducer.js
+++ react web and database/WEB-CRUD/src/service/reducer.js
@@ -1,26 +1,34 @@
-import { intialValue, allFetchs } from "./service"
+import { initialValue, allFetchs } from "./service";
 
-const rootReducer = (state = intialValue,action) => {
-    switch(action.type){
-        case allFetchs.fetch_user_request :
-            return {
-                ...state,loading:true,
-            }
-        case allFetchs.fetch_user_success:
-            return {
-                loading:false,user:action.payload,error:""
-            }  
-        case allFetchs.fetch_user_failure:
-            return {
-                loading:false,user:[''],error:action.payload
-            }  
-        default :
-        return state        
-    }
-}
+const rootReducer = (state = initialValue, action) => {
+  switch (action.type) {
+
+    case allFetchs.fetch_user_request:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case allFetchs.fetch_user_success:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload, 
+        error: null
+      };
+
+    case allFetchs.fetch_user_failure:
+      return {
+        ...state,
+        loading: false,
+        users: [], 
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
 export default rootReducer;
-
-export const fetchuserrequest = () => ({type:allFetchs.fetch_user_request});
-export const fetchusersuccess = user => ({type:allFetchs.fetch_user_success,payload:user});
-export const fetchuserfailure = error => ({type:allFetchs.fetch_user_failure,payload:error});
-
