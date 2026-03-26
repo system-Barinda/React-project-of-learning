cb63a8803a44aca20b1dd4a18153664efeeda7cc
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 11:38:47 +0200

    done

--- react web and database/WEB-CRUD/src/components/DashBoardRedux.jsx
+++ react web and database/WEB-CRUD/src/components/DashBoardRedux.jsx
@@ -1,8 +1,17 @@
 export default function DashBoardRedux(){
+
+const intialValue = {
+    loading:false,
+    users:[],
+    error:""
+};
+
+
+
     return(
         <>
         <h1>the page of dashiboard</h1>
-        <Strong>the content of this page</Strong>
+        <div>the content of this page</div>
         </>
     )
 }
