c3fcb76fd82acbabbfa481b8cd122666ce76e40f
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 11:41:44 +0200

    done

--- react web and database/WEB-CRUD/src/components/DashBoardRedux.jsx
+++ react web and database/WEB-CRUD/src/components/DashBoardRedux.jsx
@@ -1,11 +1,9 @@
+import { intialValue } from "../service/service";
 export default function DashBoardRedux(){
 
-const intialValue = {
-    loading:false,
-    users:[],
-    error:""
-};
-
+const fetch_user_request = 'fetch_user_request';
+const fetch_user_success = 'fetch_user_success';
+const fetch_user_failure = 'fetch_user_failure';
 
 
     return(
---
+++ react web and database/WEB-CRUD/src/service/service.js
@@ -0,0 +1,5 @@
+export const intialValue = {
+    loading:false,
+    users:[],
+    error:""
+};
