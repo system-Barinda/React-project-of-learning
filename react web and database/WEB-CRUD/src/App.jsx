30c65dfed1f2a8e8c990cb56d5de33f35c39ab5c
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-26 14:30:26 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,15 +1,6 @@
-import DashBoardRedux from "./components/DashBoardRedux";
-import GetAllData from "./components/GetAllData";
-import store from "./service/store";
-import { Provider } from "react-redux";
-
 function App(){
-  return (
-    <Provider store={store}>
-      <DashBoardRedux />
-      <GetAllData /> 
-    </Provider>
-  );
+  return(
+    <h1>barinda</h1>
+  )
 }
-
 export default App;
---
+++ react web and database/WEB-CRUD/src/contextApi/UserContext.jsx
@@ -0,0 +1,5 @@
+export default function UserContext (){
+    return(
+        <h1>the look</h1>
+    )
+}
