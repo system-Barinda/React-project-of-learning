80636719eaa0c7ed013dba259e130e5534b1b5f7
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-27 15:17:23 +0200

    done

--- react web and database/WEB-CRUD/src/App.jsx
+++ react web and database/WEB-CRUD/src/App.jsx
@@ -1,10 +1,32 @@
-function App(){
-  return(
+import { css } from "../styled-system/css";
+
+function App() {
+  const todos = [
+  {
+    id: 1,
+    text: "Learn React",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Build a Todo App",
+    completed: true,
+  },
+];
+  return (
     <>
-    
+      <div
+        className={css({
+          height: "100px",
+          width: "300px",
+          bg: "green.600",
+        })}
+      >
+
+       
+      </div>
     </>
-  )
+  );
 }
+
 export default App;
-
-
